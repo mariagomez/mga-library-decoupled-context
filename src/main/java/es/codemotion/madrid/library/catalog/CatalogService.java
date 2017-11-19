282333e6e81ee3b3b5aa00656979df6e34bb62af
Jorge Agudo Praena <joragupra@gmail.com>
2017-11-19 20:24:45 +0100

    Complete extraction of getAllBooks

--- src/main/java/es/codemotion/madrid/library/catalog/CatalogService.java
+++ src/main/java/es/codemotion/madrid/library/catalog/CatalogService.java
@@ -1,12 +1,14 @@
 package es.codemotion.madrid.library.catalog;
 
+import es.codemotion.madrid.library.dao.Item;
 import es.codemotion.madrid.library.models.Book;
 import es.codemotion.madrid.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class CatalogService {
@@ -15,6 +17,11 @@ public class CatalogService {
     private BookRepository bookRepository;
 
     public List<Book> getAllBooks() {
-        return new ArrayList<>();
+        Iterable<Item> items = bookRepository.findAll();
+        List<Book> books = StreamSupport.stream(items.spliterator(), false)
+                .map(item -> new Book(item.getId(), item.getName(), item.getAuthor(), item.getDescription(), item.getRating(),
+                        item.getImagePath(), item.isAvailable()))
+                .collect(Collectors.toList());
+        return books;
     }
 }

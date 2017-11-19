2b787832de468282fd745a987fa6336c95638a65
Jorge Agudo Praena <joragupra@gmail.com>
2017-11-19 21:48:29 +0100

    Complete extraction of getAvailability to BorrowService

--- src/main/java/es/codemotion/madrid/library/borrow/BorrowService.java
+++ src/main/java/es/codemotion/madrid/library/borrow/BorrowService.java
@@ -1,13 +1,18 @@
 package es.codemotion.madrid.library.borrow;
 
 import es.codemotion.madrid.library.dao.ItemAvailability;
+import es.codemotion.madrid.library.repositories.ItemAvailabilityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BorrowService {
 
+    @Autowired
+    private ItemAvailabilityRepository itemAvailabilityRepository;
+
     public ItemAvailability getAvailability(long itemId) {
-        return null;
+        return itemAvailabilityRepository.findOne(itemId);
     }
 
 }
--- src/test/java/es/codemotion/madrid/library/borrow/BorrowServiceTest.java
+++ src/test/java/es/codemotion/madrid/library/borrow/BorrowServiceTest.java
@@ -1,27 +1,36 @@
 package es.codemotion.madrid.library.borrow;
 
 import es.codemotion.madrid.library.dao.ItemAvailability;
+import es.codemotion.madrid.library.repositories.ItemAvailabilityRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BorrowServiceTest {
 
+    @Autowired
     private BorrowService borrowService;
-
+    @MockBean
+    private ItemAvailabilityRepository itemAvailabilityRepository;
     private ItemAvailability first = new ItemAvailability(true);
     private ItemAvailability second = new ItemAvailability(false);
 
     @Before
     public void setUp() {
-        borrowService = new BorrowService();
+        initMocks(this);
+        when(itemAvailabilityRepository.findOne(1L)).thenReturn(first);
+        when(itemAvailabilityRepository.findOne(2L)).thenReturn(second);
     }
 
     @Test
